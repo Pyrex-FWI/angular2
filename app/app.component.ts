99e010ed68e0d7b8d81763a7565cc66b5690fd7a
Yemistikris <yemistikris@macbook-pro-de-yemistikris-1.home>
2015-12-20 16:42:52 +0100

    angular2-tour-of-heroes-03

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,10 @@
 import {Component} from 'angular2/core';
-
-interface Hero {
-    id: number;
-    name: string;
-}
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
+    directives: [HeroDetailComponent],
     styles:[`
       .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
       .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
@@ -30,14 +28,7 @@ interface Hero {
             <span class="badge">{{hero.id}}</span> {{hero.name}}
         </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `
 })
 export class AppComponent {
